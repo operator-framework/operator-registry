b92c5b3a70374913cbc45eb4595064ce08ed12ab
njhale <njohnhale@gmail.com>
2018-12-11 16:18:23 -0500

    feat(registry-server): write panic and fatal log levels to a termination log
    - adds `termination-log` flag to `registry-server` binary to specify
    termination log path
    - writes panic and fatal log levels to a termination log

--- cmd/registry-server/main.go
+++ cmd/registry-server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net"
+	"os"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -10,6 +11,7 @@ import (
 	"google.golang.org/grpc/reflection"
 
 	"github.com/operator-framework/operator-registry/pkg/api"
+	"github.com/operator-framework/operator-registry/pkg/lib/log"
 	"github.com/operator-framework/operator-registry/pkg/server"
 	"github.com/operator-framework/operator-registry/pkg/sqlite"
 )
@@ -32,16 +34,49 @@ func main() {
 	rootCmd.Flags().Bool("debug", false, "enable debug logging")
 	rootCmd.Flags().StringP("database", "d", "bundles.db", "relative path to sqlite db")
 	rootCmd.Flags().StringP("port", "p", "50051", "port number to serve on")
+	rootCmd.Flags().StringP("termination-log", "t", "/dev/termination-log", "path to a container termination log file")
 	if err := rootCmd.Flags().MarkHidden("debug"); err != nil {
-		panic(err)
+		logrus.Panic(err.Error())
 	}
 
 	if err := rootCmd.Execute(); err != nil {
-		panic(err)
+		logrus.Panic(err.Error())
 	}
 }
 
 func runCmdFunc(cmd *cobra.Command, args []string) error {
+	// Immediately set up termination log
+	terminationLogPath, err := cmd.Flags().GetString("termination-log")
+	if err != nil {
+		return err
+	}
+	terminationLogFile, err := os.OpenFile(terminationLogPath, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	log.AddHooks(
+		&log.WriterHook{
+			Writer: terminationLogFile,
+			LogLevels: []logrus.Level{
+				logrus.PanicLevel,
+				logrus.FatalLevel,
+			},
+		},
+		&log.WriterHook{
+			Writer: os.Stderr,
+			LogLevels: []logrus.Level{
+				logrus.ErrorLevel,
+				logrus.WarnLevel,
+			},
+		},
+		&log.WriterHook{
+			Writer: os.Stdout,
+			LogLevels: []logrus.Level{
+				logrus.InfoLevel,
+				logrus.DebugLevel,
+			},
+		})
+
 	dbName, err := cmd.Flags().GetString("database")
 	if err != nil {
 		return err
@@ -52,34 +87,34 @@ func runCmdFunc(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	log := logrus.WithFields(logrus.Fields{"database": dbName, "port": port})
+	logger := logrus.WithFields(logrus.Fields{"database": dbName, "port": port})
 
 	store, err := sqlite.NewSQLLiteQuerier(dbName)
 	if err != nil {
-		log.Fatalf("failed to load db: %v", err)
+		logger.Fatalf("failed to load db: %v", err)
 	}
 
 	// sanity check that the db is available
 	tables, err := store.ListTables(context.TODO())
 	if err != nil {
-		log.Fatalf("couldn't list tables in db, incorrect config: %v", err)
+		logger.Fatalf("couldn't list tables in db, incorrect config: %v", err)
 	}
 	if len(tables) == 0 {
-		log.Fatal("no tables found in db")
+		logger.Fatal("no tables found in db")
 	}
 
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		logger.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 
 	api.RegisterRegistryServer(s, server.NewRegistryServer(store))
 	api.RegisterHealthServer(s, server.NewHealthServer())
 	reflection.Register(s)
-	log.Info("serving registry")
+	logger.Info("serving registry")
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		logger.Fatalf("failed to serve: %v", err)
 	}
 
 	return nil
