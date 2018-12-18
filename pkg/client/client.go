1bde2b502f7966e2cc1ab32a0e92181a540ef3e8
njhale <njohnhale@gmail.com>
2018-12-18 09:49:29 -0500

    fix(client): correct package used for healthcheck client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,9 +2,12 @@ package client
 
 import (
 	"context"
-	"github.com/operator-framework/operator-registry/pkg/api"
-	"github.com/operator-framework/operator-registry/pkg/registry"
+
 	"google.golang.org/grpc"
+
+	"github.com/operator-framework/operator-registry/pkg/api"
+	"github.com/operator-framework/operator-registry/pkg/api/grpc_health_v1"
+	"github.com/operator-framework/operator-registry/pkg/registry"
 )
 
 type Interface interface {
@@ -15,31 +18,31 @@ type Interface interface {
 
 type Client struct {
 	client api.RegistryClient
-	health api.HealthClient
-	conn *grpc.ClientConn
+	health grpc_health_v1.HealthClient
+	conn   *grpc.ClientConn
 }
 
 var _ Interface = &Client{}
 
 func (c *Client) GetBundleInPackageChannel(ctx context.Context, packageName, channelName string) (*registry.Bundle, error) {
-	bundle, err := c.client.GetBundleForChannel(ctx, &api.GetBundleInChannelRequest{PkgName:packageName, ChannelName: channelName})
-	if err!=nil {
+	bundle, err := c.client.GetBundleForChannel(ctx, &api.GetBundleInChannelRequest{PkgName: packageName, ChannelName: channelName})
+	if err != nil {
 		return nil, err
 	}
 	return registry.NewBundleFromStrings(bundle.CsvName, packageName, channelName, bundle.Object)
 }
 
 func (c *Client) GetReplacementBundleInPackageChannel(ctx context.Context, currentName, packageName, channelName string) (*registry.Bundle, error) {
-	bundle, err := c.client.GetBundleThatReplaces(ctx, &api.GetReplacementRequest{CsvName:currentName, PkgName:packageName, ChannelName:channelName})
-	if err!=nil {
+	bundle, err := c.client.GetBundleThatReplaces(ctx, &api.GetReplacementRequest{CsvName: currentName, PkgName: packageName, ChannelName: channelName})
+	if err != nil {
 		return nil, err
 	}
 	return registry.NewBundleFromStrings(bundle.CsvName, packageName, channelName, bundle.Object)
 }
 
 func (c *Client) GetBundleThatProvides(ctx context.Context, groupOrName, version, kind string) (*registry.Bundle, error) {
-	bundle, err := c.client.GetDefaultBundleThatProvides(ctx, &api.GetDefaultProviderRequest{GroupOrName:groupOrName, Version:version, Kind:kind})
-	if err!=nil {
+	bundle, err := c.client.GetDefaultBundleThatProvides(ctx, &api.GetDefaultProviderRequest{GroupOrName: groupOrName, Version: version, Kind: kind})
+	if err != nil {
 		return nil, err
 	}
 	parsedBundle, err := registry.NewBundleFromStrings(bundle.CsvName, bundle.PackageName, bundle.ChannelName, bundle.Object)
@@ -51,13 +54,12 @@ func (c *Client) GetBundleThatProvides(ctx context.Context, groupOrName, version
 
 func NewClient(address string) (*Client, error) {
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err!=nil {
+	if err != nil {
 		return nil, err
 	}
 	return &Client{
 		client: api.NewRegistryClient(conn),
-		health: api.NewHealthClient(conn),
-		conn: conn,
+		health: grpc_health_v1.NewHealthClient(conn),
+		conn:   conn,
 	}, nil
 }
-
