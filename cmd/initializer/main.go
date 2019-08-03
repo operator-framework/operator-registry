593e4615d7394055f5862685b6a422aacced22eb
njhale <njohnhale@gmail.com>
2019-08-03 12:07:20 -0400

    feat(initializer): add permissive option

--- cmd/initializer/main.go
+++ cmd/initializer/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
+	"github.com/operator-framework/operator-registry/pkg/registry"
 	"github.com/operator-framework/operator-registry/pkg/sqlite"
 )
 
@@ -25,6 +26,8 @@ func main() {
 	rootCmd.Flags().Bool("debug", false, "enable debug logging")
 	rootCmd.Flags().StringP("manifests", "m", "manifests", "relative path to directory of manifests")
 	rootCmd.Flags().StringP("output", "o", "bundles.db", "relative path to a sqlite file to create or overwrite")
+	rootCmd.Flags().Bool("permissive", false, "permit failures during registry build time")
+
 	if err := rootCmd.Flags().MarkHidden("debug"); err != nil {
 		panic(err)
 	}
@@ -43,16 +46,26 @@ func runCmdFunc(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-
-	dbLoader, err := sqlite.NewSQLLiteLoader(outFilename)
+	permissive, err := cmd.Flags().GetBool("permissive")
 	if err != nil {
-		logrus.Fatal(err)
+		return err
 	}
+
+	dbLoader := sqlite.NewErrorSupressingSQLLoader(outFilename)
 	defer dbLoader.Close()
 
 	loader := sqlite.NewSQLLoaderForDirectory(dbLoader, manifestDir)
 	if err := loader.Populate(); err != nil {
 		logrus.Fatal(err)
+	}
+
+	if err = registry.NewAggregate(dbLoader); err != nil {
+		// TODO: Pass LoadErrors/Database to validator(s) to determine what is permitted vs. not
+		if !permissive {
+			logrus.Fatalf("failing on load errors:\n%v", err)
+		}
+
+		logrus.Warnf("permissive mode enabled, permitting load errors:\n%v", err)
 	}
 
 	return nil
