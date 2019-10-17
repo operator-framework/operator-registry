958896d56b93549a2506c1f95c1dc2b89f8962c8
Lance Galletti <lgallett@redhat.com>
2019-10-17 09:58:36 -0400

    Adding opm cli

---
+++ cmd/opm/registry/cmd.go
@@ -0,0 +1,26 @@
+package registry
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// NewOpmRegistryCmd returns the appregistry-server command
+func NewOpmRegistryCmd() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use:   "registry",
+		Short: "interact with operator-registry database",
+		Long:  `interact with operator-registry database building, modifying and/or serving the operator-registry database`,
+
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if debug, _ := cmd.Flags().GetBool("debug"); debug {
+				logrus.SetLevel(logrus.DebugLevel)
+			}
+			return nil
+		},
+	}
+
+	rootCmd.AddCommand(newRegistryServeCmd())
+
+	return rootCmd
+}
