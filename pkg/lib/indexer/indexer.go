d3c0563d82a64d7eaa11d52e43b818cbef9a828f
kevinrizza <krizza@redhat.com>
2020-05-01 11:32:54 -0400

    Split build and push command runners
    Enable daemonless pull by default on indexes. When no build happens,
    this makes opm index add daemonless

--- pkg/lib/indexer/indexer.go
+++ pkg/lib/indexer/indexer.go
@@ -42,7 +42,8 @@ type ImageIndexer struct {
 	RegistryAdder       registry.RegistryAdder
 	RegistryDeleter     registry.RegistryDeleter
 	RegistryPruner      registry.RegistryPruner
-	ContainerTool       containertools.ContainerTool
+	BuildTool           containertools.ContainerTool
+	PullTool            containertools.ContainerTool
 	Logger              *logrus.Entry
 }
 
@@ -99,7 +100,7 @@ func (i ImageIndexer) AddToIndex(request AddToIndexRequest) error {
 		Permissive:    request.Permissive,
 		Mode:          request.Mode,
 		SkipTLS:       request.SkipTLS,
-		ContainerTool: i.ContainerTool,
+		ContainerTool: i.PullTool,
 	}
 
 	// Add the bundles to the registry
@@ -292,13 +293,13 @@ func (i ImageIndexer) getDatabaseFile(workingDir, fromIndex string) (string, err
 
 	var reg image.Registry
 	var rerr error
-	switch i.ContainerTool {
+	switch i.PullTool {
 	case containertools.NoneTool:
 		reg, rerr = containerdregistry.NewRegistry(containerdregistry.WithLog(i.Logger))
 	case containertools.PodmanTool:
 		fallthrough
 	case containertools.DockerTool:
-		reg, rerr = execregistry.NewRegistry(i.ContainerTool, i.Logger)
+		reg, rerr = execregistry.NewRegistry(i.PullTool, i.Logger)
 	}
 	if rerr != nil {
 		return "", rerr
