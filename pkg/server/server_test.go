150edca71e6ffe29fccd29256abedeb852df7f23
Ben Luddy <bluddy@redhat.com>
2020-10-27 14:34:13 -0400

    Completely populate test database before executing tests.
    These tests have been flaking with logs indicating that the time it
    takes to set up test fixtures can be longer than the timeout of the
    client used by the tests.

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -24,7 +24,7 @@ const (
 	dbName  = "test.db"
 )
 
-func server(lis net.Listener) {
+func server() *grpc.Server {
 	_ = os.Remove(dbName)
 	s := grpc.NewServer()
 
@@ -54,9 +54,7 @@ func server(lis net.Listener) {
 	}
 
 	api.RegisterRegistryServer(s, NewRegistryServer(store))
-	if err := s.Serve(lis); err != nil {
-		logrus.Fatalf("failed to serve: %v", err)
-	}
+	return s
 }
 
 func TestMain(m *testing.M) {
@@ -64,7 +62,12 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		logrus.Fatalf("failed to listen: %v", err)
 	}
-	go server(lis)
+	s := server()
+	go func() {
+		if err := s.Serve(lis); err != nil {
+			logrus.Fatalf("failed to serve: %v", err)
+		}
+	}()
 	exit := m.Run()
 	if err := os.Remove(dbName); err != nil {
 		logrus.Fatalf("couldn't remove db")
