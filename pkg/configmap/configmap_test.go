8720e3daeac4084753bb26d210b0fd6fce1432c5
Zvi Cahana <zvic@il.ibm.com>
2021-06-20 20:48:04 +0300

    Add an option to store Gzip-encoded bundle data in ConfigMaps
    Signed-off-by: Zvi Cahana <zvic@il.ibm.com>

--- pkg/configmap/configmap_test.go
+++ pkg/configmap/configmap_test.go
@@ -1,16 +1,23 @@
 package configmap
 
 import (
+	"context"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/operator-framework/operator-registry/pkg/api"
+	unstructuredlib "github.com/operator-framework/operator-registry/pkg/lib/unstructured"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+const (
+	configMapName      = "test-configmap"
+	configMapNamespace = "test-namespace"
 )
 
 func TestLoad(t *testing.T) {
@@ -40,7 +47,8 @@ func TestLoad(t *testing.T) {
 				assert.NotNil(t, objects)
 				assert.Equal(t, 1, len(objects))
 
-				unst := getUnstructured(t, objects[0])
+				unst, err := unstructuredlib.FromString(objects[0])
+				assert.NoError(t, err)
 				assert.True(t, unst.GetKind() == "Foo")
 			},
 		},
@@ -51,7 +59,8 @@ func TestLoad(t *testing.T) {
 				csvGot := bundleGot.GetCsvJson()
 				assert.NotNil(t, csvGot)
 
-				unst := getUnstructured(t, csvGot)
+				unst, err := unstructuredlib.FromString(csvGot)
+				assert.NoError(t, err)
 				assert.True(t, unst.GetName() == "first" || unst.GetName() == "second")
 			},
 		},
@@ -69,7 +78,8 @@ func TestLoad(t *testing.T) {
 			assertFunc: func(t *testing.T, bundleGot *api.Bundle) {
 				csvGot := bundleGot.GetCsvJson()
 				assert.NotNil(t, csvGot)
-				unst := getUnstructured(t, csvGot)
+				unst, err := unstructuredlib.FromString(csvGot)
+				assert.NoError(t, err)
 				assert.True(t, unst.GetName() == "kiali-operator.v1.4.2")
 
 				objects := bundleGot.GetObject()
@@ -83,7 +93,8 @@ func TestLoad(t *testing.T) {
 			assertFunc: func(t *testing.T, bundleGot *api.Bundle) {
 				csvGot := bundleGot.GetCsvJson()
 				assert.NotNil(t, csvGot)
-				unst := getUnstructured(t, csvGot)
+				unst, err := unstructuredlib.FromString(csvGot)
+				assert.NoError(t, err)
 				assert.True(t, unst.GetName() == "kiali-operator.v1.4.2")
 
 				objects := bundleGot.GetObject()
@@ -109,6 +120,56 @@ func TestLoad(t *testing.T) {
 	}
 }
 
+func TestLoadWriteRead(t *testing.T) {
+	tests := []struct {
+		name   string
+		source string
+		gzip   bool
+	}{
+		{
+			name:   "BundleUncompressed",
+			source: "testdata/bundles/etcd.0.9.2/",
+			gzip:   false,
+		},
+		{
+			name:   "BundleCompressed",
+			source: "testdata/bundles/etcd.0.9.2/",
+			gzip:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cm := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      configMapName,
+					Namespace: configMapNamespace,
+				},
+			}
+			clientset := fake.NewSimpleClientset()
+			clientset.CoreV1().ConfigMaps(configMapNamespace).Create(context.TODO(), cm, metav1.CreateOptions{})
+
+			cmLoader := NewConfigMapLoaderWithClient(configMapName, configMapNamespace, tt.source, tt.gzip, clientset)
+			err := cmLoader.Populate(1 << 20)
+			assert.NoError(t, err)
+
+			cm, err = clientset.CoreV1().ConfigMaps(configMapNamespace).Get(context.TODO(), configMapName, metav1.GetOptions{})
+			assert.NoError(t, err)
+
+			bundleLoader := NewBundleLoader()
+			bundle, err := bundleLoader.Load(cm)
+
+			expectedObjects, err := unstructuredlib.FromDir(tt.source + "manifests/")
+			assert.NoError(t, err)
+
+			bundleObjects, err := unstructuredlib.FromBundle(bundle)
+			assert.NoError(t, err)
+
+			assert.ElementsMatch(t, expectedObjects, bundleObjects)
+		})
+	}
+}
+
 func loadfromFile(t *testing.T, path string) *corev1.ConfigMap {
 	reader, err := os.Open(path)
 	require.NoError(t, err, "unable to load from file %s", path)
@@ -119,12 +180,4 @@ func loadfromFile(t *testing.T, path string) *corev1.ConfigMap {
 	require.NoError(t, err, "could not decode into configmap, file=%s", path)
 
 	return bundle
-}
-
-func getUnstructured(t *testing.T, str string) *unstructured.Unstructured {
-	dec := yaml.NewYAMLOrJSONDecoder(strings.NewReader(str), 1)
-	unst := &unstructured.Unstructured{}
-	err := dec.Decode(unst)
-	assert.NoError(t, err)
-	return unst
 }
