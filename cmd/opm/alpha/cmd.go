2a4466dccaa5f2f2ab65ec5babc482e1af3721fa
Joe Lanford <joe.lanford@gmail.com>
2021-07-06 16:12:43 -0400

    Add list subcommands for packages, channels, and bundles
    Signed-off-by: Joe Lanford <joe.lanford@gmail.com>

--- cmd/opm/alpha/cmd.go
+++ cmd/opm/alpha/cmd.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/operator-framework/operator-registry/cmd/opm/alpha/bundle"
 	initcmd "github.com/operator-framework/operator-registry/cmd/opm/alpha/init"
+	"github.com/operator-framework/operator-registry/cmd/opm/alpha/list"
 	"github.com/operator-framework/operator-registry/cmd/opm/alpha/render"
 	"github.com/operator-framework/operator-registry/cmd/opm/alpha/serve"
 	"github.com/operator-framework/operator-registry/cmd/opm/alpha/validate"
@@ -17,6 +18,6 @@ func NewCmd() *cobra.Command {
 		Short:  "Run an alpha subcommand",
 	}
 
-	runCmd.AddCommand(bundle.NewCmd(), initcmd.NewCmd(), serve.NewCmd(), render.NewCmd(), validate.NewCmd())
+	runCmd.AddCommand(bundle.NewCmd(), initcmd.NewCmd(), serve.NewCmd(), render.NewCmd(), validate.NewCmd(), list.NewCmd())
 	return runCmd
 }
