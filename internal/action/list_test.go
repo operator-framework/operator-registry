c6eae74a1bdec836b5c4ca6b225389e199166c98
Joe Lanford <joe.lanford@gmail.com>
2021-07-19 11:17:48 -0400

    improve tests
    Signed-off-by: Joe Lanford <joe.lanford@gmail.com>

--- internal/action/list_test.go
+++ internal/action/list_test.go
@@ -10,83 +10,114 @@ import (
 
 func TestListPackages(t *testing.T) {
 	type spec struct {
-		name     string
-		list     ListPackages
-		expected string
+		name        string
+		list        ListPackages
+		expectedOut string
+		expectedErr string
 	}
 
 	specs := []spec{
 		{
-			name: "Success",
-			list: ListPackages{IndexReference: "testdata/foo-index-v0.2.0-declcfg"},
-			expected: `NAME  DISPLAY NAME  DEFAULT CHANNEL
+			name: "Success/ValidIndex",
+			list: ListPackages{IndexReference: "testdata/list-index"},
+			expectedOut: `NAME  DISPLAY NAME  DEFAULT CHANNEL
+bar   Bar Operator  beta
 foo   Foo Operator  beta
 `,
+		},
+		{
+			name:        "Error/UnknownIndex",
+			list:        ListPackages{IndexReference: "unknown-index"},
+			expectedErr: `render reference "unknown-index": error resolving name : object required`,
 		},
 	}
 	for _, s := range specs {
 		t.Run(s.name, func(t *testing.T) {
 			res, err := s.list.Run(context.Background())
-			require.NoError(t, err)
+			if s.expectedErr != "" {
+				require.Nil(t, res)
+				require.EqualError(t, err, s.expectedErr)
+			} else {
+				require.NoError(t, err)
 
-			buf := &bytes.Buffer{}
-			err = res.WriteColumns(buf)
-			require.NoError(t, err)
+				buf := &bytes.Buffer{}
+				err = res.WriteColumns(buf)
+				require.NoError(t, err)
 
-			require.Equal(t, s.expected, buf.String())
+				require.Equal(t, s.expectedOut, buf.String())
+			}
 		})
 	}
 }
 
 func TestListChannels(t *testing.T) {
 	type spec struct {
-		name     string
-		list     ListChannels
-		expected string
+		name        string
+		list        ListChannels
+		expectedOut string
+		expectedErr string
 	}
 	specs := []spec{
 		{
 			name: "Success/WithPackage",
-			list: ListChannels{IndexReference: "testdata/foo-index-v0.2.0-declcfg", PackageName: "foo"},
-			expected: `PACKAGE  CHANNEL  HEAD
+			list: ListChannels{IndexReference: "testdata/list-index", PackageName: "foo"},
+			expectedOut: `PACKAGE  CHANNEL  HEAD
 foo      beta     foo.v0.2.0
 foo      stable   foo.v0.2.0
 `,
 		},
 		{
 			name: "Success/WithoutPackage",
-			list: ListChannels{IndexReference: "testdata/foo-index-v0.2.0-declcfg"},
-			expected: `PACKAGE  CHANNEL  HEAD
+			list: ListChannels{IndexReference: "testdata/list-index"},
+			expectedOut: `PACKAGE  CHANNEL  HEAD
+bar      beta     bar.v0.2.0
+bar      stable   bar.v0.2.0
 foo      beta     foo.v0.2.0
 foo      stable   foo.v0.2.0
 `,
+		},
+		{
+			name:        "Error/UnknownIndex",
+			list:        ListChannels{IndexReference: "unknown-index"},
+			expectedErr: `render reference "unknown-index": error resolving name : object required`,
+		},
+		{
+			name:        "Error/UnknownPackage",
+			list:        ListChannels{IndexReference: "testdata/list-index", PackageName: "unknown"},
+			expectedErr: `package "unknown" not found`,
 		},
 	}
 	for _, s := range specs {
 		t.Run(s.name, func(t *testing.T) {
 			res, err := s.list.Run(context.Background())
-			require.NoError(t, err)
+			if s.expectedErr != "" {
+				require.Nil(t, res)
+				require.EqualError(t, err, s.expectedErr)
+			} else {
+				require.NoError(t, err)
 
-			buf := &bytes.Buffer{}
-			err = res.WriteColumns(buf)
-			require.NoError(t, err)
+				buf := &bytes.Buffer{}
+				err = res.WriteColumns(buf)
+				require.NoError(t, err)
 
-			require.Equal(t, s.expected, buf.String())
+				require.Equal(t, s.expectedOut, buf.String())
+			}
 		})
 	}
 }
 
 func TestListBundles(t *testing.T) {
 	type spec struct {
-		name     string
-		list     ListBundles
-		expected string
+		name        string
+		list        ListBundles
+		expectedOut string
+		expectedErr string
 	}
 	specs := []spec{
 		{
 			name: "Success/WithPackage",
-			list: ListBundles{IndexReference: "testdata/foo-index-v0.2.0-declcfg", PackageName: "foo"},
-			expected: `PACKAGE  CHANNEL  BUNDLE      REPLACES    SKIPS                  SKIP RANGE  IMAGE
+			list: ListBundles{IndexReference: "testdata/list-index", PackageName: "foo"},
+			expectedOut: `PACKAGE  CHANNEL  BUNDLE      REPLACES    SKIPS                  SKIP RANGE  IMAGE
 foo      beta     foo.v0.1.0                                     <0.1.0      test.registry/foo-operator/foo-bundle:v0.1.0
 foo      beta     foo.v0.2.0  foo.v0.1.0  foo.v0.1.1,foo.v0.1.2  <0.2.0      test.registry/foo-operator/foo-bundle:v0.2.0
 foo      stable   foo.v0.2.0  foo.v0.1.0  foo.v0.1.1,foo.v0.1.2  <0.2.0      test.registry/foo-operator/foo-bundle:v0.2.0
@@ -94,24 +125,42 @@ foo      stable   foo.v0.2.0  foo.v0.1.0  foo.v0.1.1,foo.v0.1.2  <0.2.0      tes
 		},
 		{
 			name: "Success/WithoutPackage",
-			list: ListBundles{IndexReference: "testdata/foo-index-v0.2.0-declcfg"},
-			expected: `PACKAGE  CHANNEL  BUNDLE      REPLACES    SKIPS                  SKIP RANGE  IMAGE
+			list: ListBundles{IndexReference: "testdata/list-index"},
+			expectedOut: `PACKAGE  CHANNEL  BUNDLE      REPLACES    SKIPS                  SKIP RANGE  IMAGE
+bar      beta     bar.v0.1.0                                     <0.1.0      test.registry/bar-operator/bar-bundle:v0.1.0
+bar      beta     bar.v0.2.0  bar.v0.1.0  bar.v0.1.1,bar.v0.1.2  <0.2.0      test.registry/bar-operator/bar-bundle:v0.2.0
+bar      stable   bar.v0.2.0  bar.v0.1.0  bar.v0.1.1,bar.v0.1.2  <0.2.0      test.registry/bar-operator/bar-bundle:v0.2.0
 foo      beta     foo.v0.1.0                                     <0.1.0      test.registry/foo-operator/foo-bundle:v0.1.0
 foo      beta     foo.v0.2.0  foo.v0.1.0  foo.v0.1.1,foo.v0.1.2  <0.2.0      test.registry/foo-operator/foo-bundle:v0.2.0
 foo      stable   foo.v0.2.0  foo.v0.1.0  foo.v0.1.1,foo.v0.1.2  <0.2.0      test.registry/foo-operator/foo-bundle:v0.2.0
 `,
 		},
+		{
+			name:        "Error/UnknownIndex",
+			list:        ListBundles{IndexReference: "unknown-index"},
+			expectedErr: `render reference "unknown-index": error resolving name : object required`,
+		},
+		{
+			name:        "Error/UnknownPackage",
+			list:        ListBundles{IndexReference: "testdata/list-index", PackageName: "unknown"},
+			expectedErr: `package "unknown" not found`,
+		},
 	}
 	for _, s := range specs {
 		t.Run(s.name, func(t *testing.T) {
 			res, err := s.list.Run(context.Background())
-			require.NoError(t, err)
+			if s.expectedErr != "" {
+				require.Nil(t, res)
+				require.EqualError(t, err, s.expectedErr)
+			} else {
+				require.NoError(t, err)
 
-			buf := &bytes.Buffer{}
-			err = res.WriteColumns(buf)
-			require.NoError(t, err)
+				buf := &bytes.Buffer{}
+				err = res.WriteColumns(buf)
+				require.NoError(t, err)
 
-			require.Equal(t, s.expected, buf.String())
+				require.Equal(t, s.expectedOut, buf.String())
+			}
 		})
 	}
 }
