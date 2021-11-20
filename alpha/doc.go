5f8e1c1e0157930db12ce462b76c6b8de270a424
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-21 07:44:26 +0800

    Delete alpha directory

--- alpha/doc.go
+++
@@ -1,9 +0,0 @@
-// Package alpha contains subpackages that have unstable Go APIs.
-// Subpackages that achieve maturity will be deprecated and moved
-// to other modules or packages within operator-registry.
-//
-// Presence in the alpha package should not be construed as being
-// of less quality than non-alpha packages. Subpackages in the
-// alpha package have the same standard for testing as all other
-// packages in this repository.
-package alpha
