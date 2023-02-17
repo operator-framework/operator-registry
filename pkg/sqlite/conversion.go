996cfd2925640393bcbd999dabbc04de3617f090
Rashmi Gottipati <chowdary.grashmi@gmail.com>
2023-02-17 15:12:55 -0500

    exclude bundles with `olm.deprecated` property when rendering
    Signed-off-by: Rashmi Gottipati <chowdary.grashmi@gmail.com>

--- pkg/sqlite/conversion.go
+++ pkg/sqlite/conversion.go
@@ -76,7 +76,19 @@ func populateModelChannels(ctx context.Context, pkgs model.Model, q *SQLQuerier)
 	if err != nil {
 		return err
 	}
+	deprecatedValue, err := json.Marshal(registry.DeprecatedProperty{})
+	if err != nil {
+		return err
+	}
+ConvertBundles:
 	for _, bundle := range bundles {
+		for _, prop := range bundle.Properties {
+			if prop.Type == registry.DeprecatedType && prop.Value == string(deprecatedValue) {
+				// bundle contains `olm.Deprecated` property
+				// exclude this bundle from being rendered
+				continue ConvertBundles
+			}
+		}
 		pkg, ok := pkgs[bundle.PackageName]
 		if !ok {
 			return fmt.Errorf("unknown package %q for bundle %q", bundle.PackageName, bundle.CsvName)
