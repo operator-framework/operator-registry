ca611d89a80dd70e008a61174c7c581f3af40783
Jordan Keister <jordan@nimblewidget.com>
2024-06-12 16:32:58 -0500

    two-flag approach (with cleanup of old dockerfile)
    Signed-off-by: Jordan Keister <jordan@nimblewidget.com>

--- alpha/action/generate_dockerfile_test.go
+++ alpha/action/generate_dockerfile_test.go
@@ -41,88 +41,9 @@ func TestGenerateDockerfile(t *testing.T) {
 		{
 			name: "Success/WithoutExtraLabels",
 			gen: GenerateDockerfile{
-				BaseImage: "foo",
-				IndexDir:  "bar",
-			},
-			expectedDockerfile: `# The base image is expected to contain
-# /bin/opm (with a serve subcommand) and /bin/grpc_health_probe
-FROM foo
-
-# Configure the entrypoint and command
-ENTRYPOINT ["/bin/opm"]
-CMD ["serve", "/configs", "--cache-dir=/tmp/cache"]
-
-# Copy declarative config root into image at /configs and pre-populate serve cache
-ADD bar /configs
-RUN ["/bin/opm", "serve", "/configs", "--cache-dir=/tmp/cache", "--cache-only"]
-
-# Set DC-specific label for the location of the DC root directory
-# in the image
-LABEL operators.operatorframework.io.index.configs.v1=/configs
-`,
-		},
-		{
-			name: "Success/WithExtraLabels",
-			gen: GenerateDockerfile{
-				BaseImage: "foo",
-				IndexDir:  "bar",
-				ExtraLabels: map[string]string{
-					"key1": "value1",
-					"key2": "value2",
-				},
-			},
-			expectedDockerfile: `# The base image is expected to contain
-# /bin/opm (with a serve subcommand) and /bin/grpc_health_probe
-FROM foo
-
-# Configure the entrypoint and command
-ENTRYPOINT ["/bin/opm"]
-CMD ["serve", "/configs", "--cache-dir=/tmp/cache"]
-
-# Copy declarative config root into image at /configs and pre-populate serve cache
-ADD bar /configs
-RUN ["/bin/opm", "serve", "/configs", "--cache-dir=/tmp/cache", "--cache-only"]
-
-# Set DC-specific label for the location of the DC root directory
-# in the image
-LABEL operators.operatorframework.io.index.configs.v1=/configs
-
-# Set other custom labels
-LABEL "key1"="value1"
-LABEL "key2"="value2"
-`,
-		},
-
-		{
-			name: "Lite/Fail/EmptyBaseImage",
-			gen: GenerateDockerfile{
-				IndexDir: "bar",
-				ExtraLabels: map[string]string{
-					"key1": "value1",
-					"key2": "value2",
-				},
-				Lite: true,
-			},
-			expectedErr: "base image is unset",
-		},
-		{
-			name: "Lite/Fail/EmptyFromDir",
-			gen: GenerateDockerfile{
-				BaseImage: "foo",
-				ExtraLabels: map[string]string{
-					"key1": "value1",
-					"key2": "value2",
-				},
-				Lite: true,
-			},
-			expectedErr: "index directory is unset",
-		},
-		{
-			name: "Lite/Success/WithoutExtraLabels",
-			gen: GenerateDockerfile{
-				BaseImage: "foo",
-				IndexDir:  "bar",
-				Lite:      true,
+				BuilderImage: "foo",
+				BaseImage:    "foo",
+				IndexDir:     "bar",
 			},
 			expectedDockerfile: `# The builder image is expected to contain
 # /bin/opm (with serve subcommand)
@@ -132,7 +53,13 @@ FROM foo as builder
 ADD bar /configs
 RUN ["/bin/opm", "serve", "/configs", "--cache-dir=/tmp/cache", "--cache-only"]
 
-FROM scratch
+FROM foo
+# The base image is expected to contain
+# /bin/opm (with serve subcommand) and /bin/grpc_health_probe
+
+# Configure the entrypoint and command
+ENTRYPOINT ["/bin/opm"]
+CMD ["serve", "/configs", "--cache-dir=/tmp/cache"]
 
 COPY --from=builder /configs /configs
 COPY --from=builder /tmp/cache /tmp/cache
@@ -143,15 +70,75 @@ LABEL operators.operatorframework.io.index.configs.v1=/configs
 `,
 		},
 		{
-			name: "Lite/Success/WithExtraLabels",
+			name: "Success/WithExtraLabels",
 			gen: GenerateDockerfile{
-				BaseImage: "foo",
-				IndexDir:  "bar",
+				BuilderImage: "foo",
+				BaseImage:    "foo",
+				IndexDir:     "bar",
 				ExtraLabels: map[string]string{
 					"key1": "value1",
 					"key2": "value2",
 				},
-				Lite: true,
+			},
+			expectedDockerfile: `# The builder image is expected to contain
+# /bin/opm (with serve subcommand)
+FROM foo as builder
+
+# Copy FBC root into image at /configs and pre-populate serve cache
+ADD bar /configs
+RUN ["/bin/opm", "serve", "/configs", "--cache-dir=/tmp/cache", "--cache-only"]
+
+FROM foo
+# The base image is expected to contain
+# /bin/opm (with serve subcommand) and /bin/grpc_health_probe
+
+# Configure the entrypoint and command
+ENTRYPOINT ["/bin/opm"]
+CMD ["serve", "/configs", "--cache-dir=/tmp/cache"]
+
+COPY --from=builder /configs /configs
+COPY --from=builder /tmp/cache /tmp/cache
+
+# Set FBC-specific label for the location of the FBC root directory
+# in the image
+LABEL operators.operatorframework.io.index.configs.v1=/configs
+
+# Set other custom labels
+LABEL "key1"="value1"
+LABEL "key2"="value2"
+`,
+		},
+
+		{
+			name: "Scratch/Fail/EmptyBaseImage",
+			gen: GenerateDockerfile{
+				BuilderImage: "foo",
+				IndexDir:     "bar",
+				ExtraLabels: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+			},
+			expectedErr: "base image is unset",
+		},
+		{
+			name: "Scratch/Fail/EmptyFromDir",
+			gen: GenerateDockerfile{
+				BuilderImage: "foo",
+				BaseImage:    "scratch",
+				ExtraLabels: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+			},
+			expectedErr: "index directory is unset",
+		},
+		{
+			name: "Scratch/Success/WithoutExtraLabels",
+			gen: GenerateDockerfile{
+				BuilderImage: "foo",
+				BaseImage:    "scratch",
+				IndexDir:     "bar",
 			},
 			expectedDockerfile: `# The builder image is expected to contain
 # /bin/opm (with serve subcommand)
@@ -162,6 +149,47 @@ ADD bar /configs
 RUN ["/bin/opm", "serve", "/configs", "--cache-dir=/tmp/cache", "--cache-only"]
 
 FROM scratch
+# OLMv0 CatalogSources that use binary-less images must set:
+# spec:
+#   grpcPodConfig:
+#     extractContent:
+#       catalogDir: /configs
+#       cacheDir: /tmp/cache
+
+COPY --from=builder /configs /configs
+COPY --from=builder /tmp/cache /tmp/cache
+
+# Set FBC-specific label for the location of the FBC root directory
+# in the image
+LABEL operators.operatorframework.io.index.configs.v1=/configs
+`,
+		},
+		{
+			name: "Scratch/Success/WithExtraLabels",
+			gen: GenerateDockerfile{
+				BuilderImage: "foo",
+				BaseImage:    "scratch",
+				IndexDir:     "bar",
+				ExtraLabels: map[string]string{
+					"key1": "value1",
+					"key2": "value2",
+				},
+			},
+			expectedDockerfile: `# The builder image is expected to contain
+# /bin/opm (with serve subcommand)
+FROM foo as builder
+
+# Copy FBC root into image at /configs and pre-populate serve cache
+ADD bar /configs
+RUN ["/bin/opm", "serve", "/configs", "--cache-dir=/tmp/cache", "--cache-only"]
+
+FROM scratch
+# OLMv0 CatalogSources that use binary-less images must set:
+# spec:
+#   grpcPodConfig:
+#     extractContent:
+#       catalogDir: /configs
+#       cacheDir: /tmp/cache
 
 COPY --from=builder /configs /configs
 COPY --from=builder /tmp/cache /tmp/cache
